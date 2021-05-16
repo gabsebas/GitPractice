143c4598b47e2acffefca49a4579eaf263dd853b
Gabriela Sebastian <smolgaby27@gmail.com>
2021-05-16 12:26:03 -0500

    Initial Commit

---
+++ GitPractice/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GitPractice
+//
+//  Created by Gabriela Sebastian on 5/16/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Hello world!")
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
